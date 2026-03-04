42788f6907208897a51b6af5b1346f39ff8dc9e1
christianspmt <christianreysumayang@gmail.com>
2026-03-04 15:21:36 +0800

    Automating BarCode

---
+++ Horizon Seller/Horizon Seller/Controllers/LeaderboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Horizon_Seller.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult LeaderboardDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Horizon Seller/Horizon Seller/Models/LeaderboardViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Horizon_Seller.Models
+{
+    public class LeaderboardViewModel
+    {
+    }
+}
